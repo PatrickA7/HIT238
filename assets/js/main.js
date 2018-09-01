1ca58b718cada135ce0f79bd4719276bd66d27cc
PatrickA7 <patrick.alegria7@gmail.com>
2018-09-01 20:59:53 +0930

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,37 +5,3 @@ $('#register-password-input, #register-passwordconfirm-input').on('keyup', funct
   } else 
     $('#message').html('Passwords do not match').css('color', 'red');
 });
-//code for dummy item list - remove later
-/*$('.search').keyup(function() {
-    var value = $(this).val();
-    var exp = new RegExp('^' + value, 'i');
-
-    $('.dummyItemList .item').each(function() {
-        var isMatch = exp.test($('.name', this).text());
-        $(this).toggle(isMatch);
-    });
-});
-function saveDataBread(){
-    var input = document.getElementById("bread-quantity");
-    sessionStorage.setItem("Bread", input.value);
-}
-
-function saveDataEggs(){
-    var input = document.getElementById("eggs-quantity");
-    sessionStorage.setItem("Eggs", input.value);
-}
-
-function saveDataMilk(){
-    var input = document.getElementById("milk-quantity");
-    sessionStorage.setItem("Milk", input.value);
-}
-for (var i = 0; i < sessionStorage.length; i++) {
-    var items = sessionStorage.key(i);
-    var quantity = sessionStorage.getItem(sessionStorage.key(i));
-    document.getElementById('test2').innerHTML = '<tr><td>' + items + '</td>' + '<td>' + quantity + '</td><td><button class="btn btn-danger" id="bread-button" onclick="deleteItem()">Delete</button></td></tr>';
-}
-
-function deleteItem(){
-    sessionStorage.removeItem(items);
-    location.reload();
-}*/
