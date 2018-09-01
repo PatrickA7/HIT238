e8cae954c7ec61ded3ddb461d64296a8bd2d7b10
PatrickA7 <patrick.alegria7@gmail.com>
2018-09-01 21:32:30 +0930

    Remove extra files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Booking Buddy - an app to reserve tables at your favorite rastaurant faster
 
 Project for HIT238
+
+Test account
+Email: testacc@gmail.com
+Password: tester123
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,16 @@ $('#register-password-input, #register-passwordconfirm-input').on('keyup', funct
   } else 
     $('#message').html('Passwords do not match').css('color', 'red');
 });
+<<<<<<< HEAD
+//nav items
+var navItems = document.querySelectorAll(".mobile-bottom-nav__item");
+navItems.forEach(function(e, i) {
+	e.addEventListener("click", function(e) {
+		navItems.forEach(function(e2, i2) {
+			e2.classList.remove("mobile-bottom-nav__item--active");
+		})
+		this.classList.add("mobile-bottom-nav__item--active");
+	});
+});
+=======
+>>>>>>> 067735b07c0b21e4d7ec3de371768bd5c4bcdf2d
